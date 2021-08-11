f24d832383439c7f7c49900525fcd06198db3b09
monirulislamweb <65311288+monirulislamweb@users.noreply.github.com>
2021-08-11 21:10:35 +0600

    ...

--- themes/stevendw/assets/js/main.js
+++ themes/stevendw/assets/js/main.js
@@ -225,6 +225,32 @@ google.maps.event.addDomListener(window, 'load', initialize);
   }, false);
 })();
 
+
+
+
+
+  /*start of Noyon*/
+
+
+
+
+  /*start of Shariful*/
+
+
+
+
+
+  /*start of Keshob*/
+
+
+
+
+
+
+
+
+
+
     new WOW().init();
 
 })(jQuery);
