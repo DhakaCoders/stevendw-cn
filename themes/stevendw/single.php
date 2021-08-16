83c4d5e7e2bbf34d1153a1bcc1a943e29ba2e40e
Shariful700453 <56506215+Shariful700453@users.noreply.github.com>
2021-08-16 12:30:43 +0600

    ,

--- themes/stevendw/single.php
+++ themes/stevendw/single.php
@@ -303,7 +303,9 @@ $banner = !empty($imgID)? cbv_get_image_src($imgID): banner_placeholder();
                       <?php if( !empty($fc_titel) ) printf( '<h2 class="title-desc-module fl-h2">%s</h2>', $fc_titel ); ?>
                     </div>
                     <div class="title-desc-module-rgt">
-                      <?php if( !empty($fc_sols['fc_tekst']) ) echo wpautop($fc_sols['fc_tekst']); ?>
+                      <div>
+                        <?php if( !empty($fc_sols['fc_tekst']) ) echo wpautop($fc_sols['fc_tekst']); ?>
+                      </div>
                     </div>
                   </div>
                 </div>
